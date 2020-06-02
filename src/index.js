9f0951d7c5a331c40deba8d5cfa7ec442be6452e
ThanhNguyen <thanh.nguyen@saritasa.com>
2020-06-02 10:24:57 +0700

    Command: npx webpack, build the basic bundle

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack - npm'], ' ');
+
+    return element;
+  }
+
+  document.body.appendChild(component());
